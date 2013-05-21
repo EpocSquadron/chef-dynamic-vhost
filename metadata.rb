5e53c18bdc211b8cf49cb95cfaa2cb00e6527287
Daniel Poulin <dan.poulin@digitalsurgeons.com>
2013-05-21 15:49:22 -0400

    Satisfy foodcritic

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
 name             "chef-dynamic-vhost"
-maintainer       "YOUR_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "Daniel Poulin"
+maintainer_email "crimsonmage@gmail.com"
 license          "All rights reserved"
-description      "Installs/Configures chef-dynamic-vhost"
+description      "Configures various web servers to serve wildcard domains for development setups."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
-
+depends          "apache2"
+depends          "php"
